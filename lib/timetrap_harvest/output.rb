da67005c36168f9a6c79e104bae92fb6e92617ff
Devon Blandin <dblandin@gmail.com>
2014-06-23 10:50:07 -0500

    Break up output sections with empty lines

--- lib/timetrap_harvest/output.rb
+++ lib/timetrap_harvest/output.rb
@@ -15,18 +15,22 @@ class HarvestOutput
     unless submitted.empty?
       messages << SUBMITTED_HEADER
       messages += submitted.map { |submitted| success_message(submitted[:notes]) }
+      messages << "\n"
     end
 
     unless failed.empty?
       messages << FAILED_HEADER
       messages += failed.map { |failed| failed_message(failed[:note], failed[:error]) }
+      messages << "\n"
     end
 
     messages.join("\n")
   end
 
+  private
+
   def stats
-    "Submitted: #{submitted.count}\nFailed: #{failed.count}"
+    "Submitted: #{submitted.count}\nFailed: #{failed.count}\n"
   end
 
   def submitted
